a8dfd20256d16f6605366bf73cf26eeff200eaff
Nick Taylor <nick@nickyt.co>
2023-09-28 14:01:45 -0400

    chore: added demo edge functions

---
+++ netlify/edge-functions/geo/geo.ts
@@ -0,0 +1,8 @@
+import { Context } from "https://edge.netlify.com";
+
+export default async (request: Request, context: Context) => {
+  return Response.json({
+    geo: context.geo,
+    header: request.headers.get("x-nf-geo"),
+  });
+};
